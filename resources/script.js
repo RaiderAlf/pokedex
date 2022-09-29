4cab31ccf515074f417d48cdfe5023beb3670458
Kevin Alfonzo <djdrakomusica@gmail.com>
2022-09-28 21:38:45 -0430

    fetch

--- resources/script.js
+++ resources/script.js
@@ -1,14 +1,38 @@
+const pokemonContainer = document.querySelector(".contentApi");
 
-fetch('https://pokeapi.co/api/v2/ability/7/')
-    .then(response => response.json())
-    .then(data => mostrarApi(data))
-    .catch(error => console.log(error))
-    
-    let mostrarApi = (data) => {
-        let placeApi = '';
-        console.log(data)
-        data.forEach(function(p, i){
-            placeApi += `<span>${data.p}`
-        });
-    document.getElementById('data').innerHTML = placeApi
+function fetchPokemon(id){
+    fetch(`https://pokeapi.co/api/v2/pokemon/${id}/`)
+        .then(response => response.json())
+        .then((data) => createPokemon(data))
+};
+
+function fetchPoke(number) {
+    for(let i = 1; i <= number; i++){
+        fetchPokemon(i)
+    } 
+};
+
+function createPokemon(pokemon) {
+    const card = document.createElement('div');
+    card.classList.add('pokemon-block');
+
+    const spriteContainer = document.createElement('div');
+    spriteContainer.classList.add('img-container');
+
+    const sprite = document.createElement('img');
+    sprite.src = pokemon.sprites.front_default;
+
+    spriteContainer.appendChild(sprite)
+
+    const number = document.createElement('p');
+    number.textContent = `#${pokemon.id.toString().padStart(3, 0)}`;
+
+    const name = document.createElement('p');
+    name.classList.add('name');
+    name.textContent = pokemon.name;
+
+    card.append(spriteContainer, number, name);
+    pokemonContainer.appendChild(card);
 }
+
+fetchPoke(4)
