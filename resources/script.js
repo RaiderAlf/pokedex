6fb33baf2265560fe7d2bcd8764f2cfb2a99b899
Kevin Alfonzo <djdrakomusica@gmail.com>
2022-10-03 18:34:18 -0430

    pagination

--- resources/script.js
+++ resources/script.js
@@ -1,18 +1,48 @@
 const pokemonContainer = document.querySelector(".contentApi");
+const spinner = document.querySelector("#spinner");
+const searchInput = document.querySelector("#search");
+const previous = document.querySelector(".previous");
+const next = document.querySelector('.next');
+
+let offset = 1;
+let limit = 8;
+
+window.onload = () => {
+    fetchPoke(offset, limit)
+}
+
+previous.addEventListener('click', () => {
+   if(offset >= 9){
+    offset -= 8;
+    pokemonContainer.innerHTML = " ";
+    fetchPoke(offset, limit)
+   }
+})
+
+next.addEventListener('click', () => {
+    offset += 8;
+    pokemonContainer.innerHTML = " ";
+    fetchPoke(offset, limit)
+
+})
 
 function fetchPokemon(id){
     fetch(`https://pokeapi.co/api/v2/pokemon/${id}/`)
         .then(response => response.json())
-        .then((data) => createPokemon(data))
+        .then((data) => {
+            createPokemon(data);
+            spinner.style.display = "none"
+        })
 };
 
-function fetchPoke(number) {
-    for(let i = 1; i <= number; i++){
+
+function fetchPoke(offset, limit) {
+    spinner.style.display = "none"
+    for(let i = offset; i <= offset + limit; i++){
         fetchPokemon(i)
     } 
 };
 
-const searchInput = document.querySelector("#search");
 
 function createPokemon(pokemon) {
     const card = document.createElement('div');
@@ -46,5 +76,3 @@ function createPokemon(pokemon) {
     card.append(spriteContainer, number, name);
     pokemonContainer.appendChild(card);
 }
-
-fetchPoke(18)
